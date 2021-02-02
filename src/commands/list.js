5a42e09dbea0ebc113158164c7ec5984c0bbb663
Martin Didriksen <martindidriksen@hotmail.com>
2021-02-02 20:26:15 +0100

    Requested changes from review

--- src/commands/list.js
+++ src/commands/list.js
@@ -5,9 +5,7 @@ module.exports = {
     description: 'List available stations',
     execute(message) {
         const guild = message.client.guildInstances.get(message.guild.id);
-        if (!message.member.voice.channel) 
-            return message.channel.send('You have to be in a voice channel to list the channels!');
-        if (!guild || !guild.connection || !guild.connection.dispatcher) return;
+        if (!guild || !guild.textChannel) return;
         
         const channels = fs.readdirSync(__dirname + '/../../assets/channels/')
         guild.textChannel.send(channels) 
