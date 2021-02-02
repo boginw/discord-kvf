4772a02e8474f6b83b52df05d986bd2830c0d393
Martin Didriksen <martindidriksen@hotmail.com>
2021-02-02 17:24:56 +0100

    Changed text

--- src/commands/list.js
+++ src/commands/list.js
@@ -6,7 +6,7 @@ module.exports = {
     execute(message) {
         const guild = message.client.guildInstances.get(message.guild.id);
         if (!message.member.voice.channel) 
-            return message.channel.send('You have to be in a voice channel to stop the music!');
+            return message.channel.send('You have to be in a voice channel to list the channels!');
         if (!guild || !guild.connection || !guild.connection.dispatcher) return;
         
         const channels = fs.readdirSync(__dirname + '/../../assets/channels/')
