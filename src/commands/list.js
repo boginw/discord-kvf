3a4bfeca920ae739f6208170c1307b6483c151f1
Martin Didriksen <martindidriksen@hotmail.com>
2021-02-02 17:23:36 +0100

    added a list a stations command

---
+++ src/commands/list.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+module.exports = {
+    name: 'list',
+    description: 'List available stations',
+    execute(message) {
+        const guild = message.client.guildInstances.get(message.guild.id);
+        if (!message.member.voice.channel) 
+            return message.channel.send('You have to be in a voice channel to stop the music!');
+        if (!guild || !guild.connection || !guild.connection.dispatcher) return;
+        
+        const channels = fs.readdirSync(__dirname + '/../../assets/channels/')
+        guild.textChannel.send(channels) 
+    },
+};
